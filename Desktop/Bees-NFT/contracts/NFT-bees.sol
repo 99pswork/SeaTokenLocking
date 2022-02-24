669627773f9902595589863a3c09ec00cf3b939d
Pranjal <99ps.work@gmail.com>
2022-02-25 01:24:15 +0530

    Added Functionality

--- Desktop/Bees-NFT/contracts/NFT-bees.sol
+++ Desktop/Bees-NFT/contracts/NFT-bees.sol
@@ -32,25 +32,35 @@ contract BeesNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
     uint256 public preSalePrice; // 0.15ETH
     uint256 public publicSalePrice; // 0.2ETH
     uint256 public preSaleTotal; // 880 
+    uint256 public currentPreSale;
 
     uint public maxPreSale; // 1
     uint public maxPublicSale; // 5
 
     string private _baseURIextended;
+    
+    string public NETWORK_PROVENANCE = "";
+    string public notRevealerUri;
+
+    uint256 public donationAmount; // 3 ETH
+    address public charityBeesAddress; // Set Address
 
     mapping(address => bool) isWhiteListed; 
 
-    constructor(string memory name, string memory symbol, uint256 _preSalePrice, uint256 _publicSalePrice, uint256 _maxPreSale, uint256 _maxPublicSale, uint256 _preSaleTotal) ERC721(name, symbol) ReentrancyGuard() {
+    constructor(string memory name, string memory symbol, uint256 _preSalePrice, uint256 _publicSalePrice, uint256 _maxPreSale, uint256 _maxPublicSale, uint256 _preSaleTotal, uint256 _donationAmount, address _charityBeesAddress) ERC721(name, symbol) ReentrancyGuard() {
         preSalePrice = _preSalePrice;
         publicSalePrice = _publicSalePrice;
         maxPreSale = _maxPreSale;
         maxPublicSale = _maxPublicSale;
         preSaleTotal = _preSaleTotal;
+        donationAmount = _donationAmount;
+        charityBeesAddress = _charityBeesAddress;
     }
 
     function preSaleMint(uint256 _amount) external payable nonReentrant{
         require(preSaleActive, "NFT-Bees Pre Sale is not Active");
         require(isWhiteListed[msg.sender], "NFT-Bees Message Sender is not whitelisted");
+        require(currentPreSale <= preSaleTotal, "NFT-Bees Pre Sale Max Limit Reached");
         require(balanceOf(msg.sender).add(_amount) < maxPreSale, "NFT-Bees Max Pre Sale Mint Reached");
         mint(_amount, true);
     }
@@ -100,6 +110,46 @@ contract BeesNFT is ERC721Enumerable, Ownable, ReentrancyGuard {
 
     function togglePublicSale() external onlyOwner {
         publicSaleActive = !publicSaleActive;
+    }
+
+    function setPreSalePrice(uint256 _preSalePrice) external onlyOwner {
+        preSalePrice = _preSalePrice;
+    }
+
+    function setPublicSalePrice(uint256 _publicSalePrice) external onlyOwner {
+        publicSalePrice = _publicSalePrice;
+    }
+
+    function airDrop(address[] memory _address) external onlyOwner {
+        require(totalSupply().add(_address.length) <= maxSupply, "NFT-Bees Maximum Supply Reached");
+        for(uint i=0; i < _address.length; i++){
+            _safeMint(_address[i], totalSupply().add(i));
+        }
+    }
+
+    function reveal() external onlyOwner {
+        revealed = true;
+    }
+
+    function withdraw() external onlyOwner {
+        uint balance = address(this).balance;
+        payable(msg.sender).transfer(balance);
+    }
+
+    function setProvenanceHash(string memory provenanceHash) external onlyOwner {
+        NETWORK_PROVENANCE = provenanceHash;
+    }
+
+    function setNotRevealedURI(string memory _notRevealedUri) external onlyOwner {
+        notRevealerUri = _notRevealedUri;
+    }
+
+    function setCharityAddress(address _address) external onlyOwner {
+        charityBeesAddress = _address;
+    }
+
+    function donate3ETH() internal {
+        payable(charityBeesAddress).transfer(donationAmount);
     }
 
 }
